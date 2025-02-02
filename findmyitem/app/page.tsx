2a90e208b777fb807be50dede2148f56d903116c
garvsl <garvsehgal7@gmail.com>
2025-02-02 10:47:11 -0500

    put in server and db

--- findmyitem/app/page.tsx
+++ findmyitem/app/page.tsx
@@ -1,101 +1,290 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import React, { useEffect, useState } from "react";
+import {
+  PieChart,
+  Pie,
+  Cell,
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+
+const COLORS = [
+  "#0088FE",
+  "#00C49F",
+  "#FFBB28",
+  "#FF8042",
+  "#A28DFF",
+  "#FF5678",
+  "#34D399",
+  "#F97316",
+  "#E63946",
+  "#457B9D",
+  "#2A9D8F",
+  "#D4A373",
+  "#F4A261",
+  "#8D99AE",
+  "#264653",
+  "#E76F51",
+  "#6A0572",
+  "#D00000",
+  "#FF9F1C",
+  "#4A90E2",
+];
+
+export default function CarbonFootPrint() {
+  const [item, setItem] = useState<any>("");
+  const [results, setResults] = useState<any>();
+  const [loading, setLoading] = useState<any>(false);
+  const [error, setError] = useState<any>(null);
+  const [cart, setCart] = useState<any>([]);
+  const [highest, setHighest] = useState<any>(0);
+  const [highestName, setHighestName] = useState<any>("");
+
+  useEffect(() => {
+    async function getDBdata() {
+      try {
+        const response = await fetch(
+          "https://server-1sqv.onrender.com/history"
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const data = await response.json();
+        console.log("dat", data);
+
+        const today = new Date().toISOString().split("T")[0];
+        const todaysData = data.filter((entry: any) => entry.date === today);
+
+        setResults(data);
+        setCart(todaysData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    getDBdata();
+  }, []);
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    setLoading(true);
+    setError(null);
+
+    try {
+      const res = await fetch(
+        `https://server-1sqv.onrender.com/carbon?item=${item}`
+      );
+      if (!res.ok) {
+        throw new Error(`Error: ${res.statusText}`);
+      }
+      const data = await res.json();
+      console.log(data);
+
+      setResults((prevResults: any) => [...prevResults, data]);
+
+      const today = new Date().toISOString().split("T")[0];
+      if (data.date === today) {
+        setCart((prevCart: any) => [...prevCart, data]);
+      }
+
+      if (data.carbon_footprint > highest) {
+        setHighest(data.carbon_footprint);
+        setHighestName(data.product);
+      }
+    } catch (error: any) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  let pieData;
+  if (results) {
+    pieData = results.map((result: any) => ({
+      name: result.product,
+      value: result.carbon_footprint,
+    }));
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <Button className="bg-blue-500 text-white hover:bg-blue-700 px-4 py-2 rounded">
+        Carbon Footprint Calculator
+      </Button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={item}
+          onChange={(e) => setItem(e.target.value)}
+          placeholder="Enter item name"
+          style={{ padding: "10px", width: "300px", borderRadius: "5px" }}
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <button
+          type="submit"
+          className="bg-blue-500"
+          style={{
+            marginLeft: "10px",
+            padding: "10px",
+            color: "white",
+            borderRadius: "5px",
+          }}
+        >
+          Submit
+        </button>
+      </form>
+      {loading && <p>Loading...</p>}
+      {error && <p style={{ color: "blue" }}>{error}</p>}
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <div style={{ marginTop: "20px" }}>
+        <h2>Results</h2>
+        <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+          {cart.length > 0 &&
+            cart.map((entry: any, index: any) => (
+              <div
+                key={index}
+                style={{
+                  display: "flex",
+                  gap: "10px",
+                  justifyContent: "center",
+                  flexWrap: "wrap",
+                }}
+              >
+                <div
+                  style={{
+                    border: "1px solid #ddd",
+                    borderRadius: "8px",
+                    padding: "20px",
+                    maxWidth: "400px",
+                    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+                    backgroundColor: "#f9f9f9",
+                  }}
+                >
+                  <p>
+                    <strong>Product:</strong> {entry.product}
+                  </p>
+                  <p>
+                    <strong>Unit of Product:</strong> {entry.unit_of_product}
+                  </p>
+                  <p>
+                    <strong>Number of Units:</strong> {entry.number_of_units}
+                  </p>
+                </div>
+
+                <div
+                  style={{
+                    border: "1px solid #ddd",
+                    borderRadius: "8px",
+                    padding: "20px",
+                    maxWidth: "400px",
+                    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+                    backgroundColor: "#f9f9f9",
+                  }}
+                >
+                  <p>
+                    <strong>Carbon Footprint:</strong>{" "}
+                    {entry.carbon_footprint + entry.unit_of_carbon}
+                  </p>
+                  <p>
+                    <strong>Sustainable Brand:</strong>{" "}
+                    {entry.sustainable_brand}
+                  </p>
+                  <p>
+                    <strong>Date:</strong> {entry.date}
+                  </p>
+                </div>
+              </div>
+            ))}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        {results && results.length > 0 && (
+          <ResponsiveContainer width="100%" height={350}>
+            <PieChart>
+              <Tooltip />
+              <Pie
+                data={pieData}
+                dataKey="value"
+                nameKey="name"
+                cx="50%"
+                cy="50%"
+                outerRadius={150}
+                fill="#8884d8"
+                label
+              >
+                {pieData.map((entry: any, index: number) => (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={COLORS[index % COLORS.length]}
+                  />
+                ))}
+              </Pie>
+            </PieChart>
+          </ResponsiveContainer>
+        )}
+
+        {results && results.length > 0 && (
+          <ResponsiveContainer width="100%" height={350}>
+            <AreaChart
+              data={results}
+              margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="product" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Area
+                type="monotone"
+                dataKey="carbon_footprint"
+                stroke="#090374"
+                fill="#1d62d2"
+              />
+            </AreaChart>
+          </ResponsiveContainer>
+        )}
+
+        {results && results.length > 0 && (
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Product</TableHead>
+                <TableHead>Unit of Product</TableHead>
+                <TableHead>Number of Units</TableHead>
+                <TableHead>Carbon Footprint</TableHead>
+                <TableHead>Unit of Carbon</TableHead>
+                <TableHead>Date</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {results.map((result: any, index: any) => (
+                <TableRow key={index}>
+                  <TableCell>{result.product}</TableCell>
+                  <TableCell>{result.unit_of_product}</TableCell>
+                  <TableCell>{result.number_of_units}</TableCell>
+                  <TableCell>{result.carbon_footprint}</TableCell>
+                  <TableCell>{result.unit_of_carbon}</TableCell>
+                  <TableCell>{result.date}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        )}
+      </div>
     </div>
   );
 }
