d059b96c4e28100bc41efa7ca04ed6c1fc4fd5bf
IshmamF <ishmamf2003@gmail.com>
2025-02-02 07:54:55 -0500

    route to show all data in db

---
+++ findmyitem/app/api/history/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+import { type NextRequest } from 'next/server'
+import dotenv from 'dotenv';
+dotenv.config();
+import database from '../carbon/route'
+
+export async function GET(
+    req: NextRequest
+  ) {
+
+  return NextResponse.json(database)
+}
